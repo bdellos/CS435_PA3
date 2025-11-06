a7523feb530641c1d4c9e82b84a3bbe727369f72
bdellos <brendan.dellos@gmail.com>
2025-11-06 14:21:28 -0700

    Some cleanup done during verification of ideal PR algorithm. Further cleanup needed.

--- src/main/java/pa3/IdealPageRank.java
+++ src/main/java/pa3/IdealPageRank.java
@@ -13,20 +13,14 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class IdealPageRank{
+public class IdealPageRank {
 
     private static final int NUM_ITERATIONS = 25;
 
-    public static void getPageRanks (JavaSparkContext sc, String linesFilePath, String titleFilePath) {
-        // for tiny tests
+    public static void getPageRanks(JavaSparkContext sc, String linesFilePath, String titleFilePath) {
+
         JavaRDD<String> titles = sc.textFile(titleFilePath);
         JavaRDD<String> lines = sc.textFile(linesFilePath);
-
-
-        // main files
-        //JavaRDD<String> titles = sc.textFile("/input/titles-sorted.txt");
-        //JavaRDD<String> lines = sc.textFile("/input/links-simple-sorted-sample.txt");
 
         // Adds indices to the titles so they can be joined with the links
         // Ex: <9, "Name of Page">
@@ -35,7 +29,7 @@ public class IdealPageRank{
         // Create list of sources and its outgoing links
         // Ex: <9, [2, 68, 318]>
         JavaPairRDD<Long, List<Long>> links = lines.mapToPair(line -> {
-            String[] parts = line.split(":");   // [0] source, [1] destinations
+            String[] parts = line.split(":"); // [0] source, [1] destinations
             Long source = Long.parseLong(parts[0].trim());
             List<Long> dests = new ArrayList<>();
             // for pages that have no outgoing links
@@ -55,52 +49,61 @@ public class IdealPageRank{
 
         links.take(5).forEach(r -> System.out.println("PageID " + r._1 + " links: " + r._2));
 
-        // For Page Rank calculation
-        long totalLinks = links.count(); //if a page has an (1) outgoing link-TH
-        long numPages = indexedTitles.count(); //total num wikipages
-        long denom = numPages; //Check if we need to get rid of this based on assignment detial, what if we end up in a "sink"
-       
-        //Init ranks so each page will start with an equal rank-TH
-        //If we do change denom or remove it, this will change from denom to totalLinks and get rid of denom variable-TH
-        JavaPairRDD<Long, Double> ranks = links.mapValues(v -> 1.0 / (double) denom); 
+        long totalLinks = links.count();
+
+        // Init ranks so each page will start with an equal rank-TH
+        // If we do change denom or remove it, this will change from denom to totalLinks
+        // and get rid of denom variable-TH
+        JavaPairRDD<Long, Double> ranks = links.mapValues(v -> 1.0 / (double) totalLinks);
+        //ranks.take(5).forEach(r -> System.out.println(r._1 + " rank: " + r._2));
 
         // Attempt to converge in NUM_ITERATIONS attempts
         for (int i = 0; i < NUM_ITERATIONS; i++) {
 
-            //Step 1
-            /*each page will share its current rank among all pages that it links to
-            so if the page rank is 1/4(0.25) with ie 5 links out, it will convert to 0.25/5 for the dest links out from it =.05*/
+            // Step 1
+            /*
+             * each page will share its current rank among all pages that it links to
+             * so if the page rank is 1/4(0.25) with ie 5 links out, it will convert to
+             * 0.25/5 for the dest links out from it =.05
+             */
             JavaPairRDD<Long, Double> contribute = links.join(ranks).flatMapToPair(kv -> {
-                Long src = kv._1; //source page id-TH
-                List<Long> dests = kv._2._1; //list of the outgoing links of the neighboring pages
-                Double rank = kv._2._2; //current ranking of the source page-TH
-                List<Tuple2<Long, Double>> results = new ArrayList<>(); //created to hold pairs of <destPage, Contributions>
-                   
-                /*if the page has actual outgoing links, distr the rank*/
-                if(!dests.isEmpty()){
-                    double shareRank = rank/dests.size(); //what will the link get once its shared-TH
-                    for(Long d: dests){
-                        results.add(new Tuple2<>(d, shareRank)); //conversionof long to a string for the Key which is a string
+                List<Long> dests = kv._2._1; // list of the outgoing links of the neighboring pages
+                Double rank = kv._2._2; // current ranking of the source page-TH
+                List<Tuple2<Long, Double>> results = new ArrayList<>(); // created to hold pairs of <destPage,
+                                                                        // Contributions>
+
+                /* if the page has actual outgoing links, distr the rank */
+                if (!dests.isEmpty()) {
+                    double shareRank = rank / dests.size(); // what will the link get once its shared-TH
+                    for (Long d : dests) {
+                        results.add(new Tuple2<>(d, shareRank));
                     }
                 }
-                    //iterator will return to spark and flatten output-TH
-                    return results.iterator();
+                // iterator will return to spark and flatten output-TH
+                return results.iterator();
 
             });
 
-            //Step2:
-            /*take the contributions which point to the same page basedon above 
-            to gather the sum of the total incoming tally each page received from the iteration*/
-            JavaPairRDD<Long, Double> newRanking = contribute.reduceByKey((a,b) -> a + b );
-               
-            //Step 3
-            //rand update for the next iter, new rank turns to input for the new cycle of the loop 
-            ranks = newRanking;   
-                
-        }
-        //check if we need to join the ranks with the indexedTitles for saving them or printing the top K (ec)-TH
+            // Step2:
+            /*
+             * take the contributions which point to the same page basedon above
+             * to gather the sum of the total incoming tally each page received from the
+             * iteration
+             */
+            JavaPairRDD<Long, Double> newRanking = contribute.reduceByKey((a, b) -> a + b);
 
-        ranks.take(5).forEach(r -> System.out.println("PageID " + r._1 + " rank: " + r._2));
+            // Step 3
+            // rand update for the next iter, new rank turns to input for the new cycle of
+            // the loop
+            ranks = newRanking;
+            // System.out.println("Iteration: " + i);
+            // ranks.take(5).forEach(r -> System.out.println(r._1 + " rank: " + r._2));
+
+        }
+        // check if we need to join the ranks with the indexedTitles for saving them or
+        // printing the top K (ec)-TH
+
+        //ranks.take(5).forEach(r -> System.out.println("PageID " + r._1 + " rank: " + r._2));
     }
 
 }
